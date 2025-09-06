910e0e4224df54e2b7d6cfe247b7dad2a63158b7
progFilthi <gamerfilthi@gmail.com>
2025-09-06 22:48:03 +0200

    Added links and proper spacing in the dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -23,7 +23,7 @@ export default function DashboardLayoutPage({
         <SiteHeader />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="flex flex-col gap-4 p-8 md:gap-6 md:p-16">
               {children}
             </div>
           </div>
